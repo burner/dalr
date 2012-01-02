e42d1cbc75c5c03089140bbc5948abbf909e7790
Robert BuRnEr Schadek <rburners@gmail.com>
2012-01-02 17:47:40 +0100

    d2 grammer

--- dalr/main.d
+++ dalr/main.d
@@ -15,13 +15,19 @@ import hurt.io.stdio;
 import hurt.util.slog;
 
 void main() {
+	// create all facilities
 	SymbolManager sm = new SymbolManager();
 	GrammerParser gp = new GrammerParser(sm);
 	ProductionManager pm = new ProductionManager(sm);
-	FileReader fr = new FileReader("examplegrammer.dlr");
+	FileReader fr = new FileReader("d2grm.dlr");
 	fr.parse();
+
+	// map the actions to the productions
 	Map!(size_t, Production) actions = new Map!(size_t, Production)(
 		ISRType.HashTable);
+
+	// for all productions in the filereader. 
+	// add them to the productionsmanager
 	for(Iterator!(Production) it = fr.getProductionIterator(); it.isValid(); 
 			it++) {
 		actions.insert(pm.insertProduction(
@@ -29,24 +35,11 @@ void main() {
 		log("%s", (*it).getProduction());
 	}
 	
-
-/*	
-	pm.insertProduction(gp.processProduction("S := N"));
-	pm.insertProduction(gp.processProduction("N := V = E"));
-	pm.insertProduction(gp.processProduction("N := E"));
-	pm.insertProduction(gp.processProduction("E := V"));
-	pm.insertProduction(gp.processProduction("V := x"));
-	pm.insertProduction(gp.processProduction("V := * E"));
-	pm.insertProduction(gp.processProduction("S := A"));
-	pm.insertProduction(gp.processProduction("A := if B then A"));
-	pm.insertProduction(gp.processProduction("A := if B then A else A"));
-	pm.insertProduction(gp.processProduction("A := a"));
-	pm.insertProduction(gp.processProduction("B := bool"));
-*/
-
+	log();
 	pm.makeLRZeroItemSets();
+	log();
 	writeLR0Graph(pm.getItemSets(), sm, pm.getProductions(), "lr0");
-	pm.makeExtendedGrammer();
+	/*pm.makeExtendedGrammer();
 	print(extendedGrammerToString(pm, sm));
 	pm.makeNormalFirstSet();
 	print(normalFirstSetToString(pm, sm));
@@ -64,17 +57,24 @@ void main() {
 	pm.makeNormalFirstSet();
 	print(normalFirstSetToString(pm, sm));
 	pm.makeExtendedFirstSet();
+	log();
 	print(extendedFirstSetToString(pm, sm));
 	pm.makeNormalFollowSet();
+	log();
 	println(normalFollowSetToString(pm, sm));
 	println(extendedGrammerItemsToString(pm, sm));
+	log();
 	pm.makeExtendedFollowSet();
+	log();
 	pm.getTranslationTable();
+	log();
 	pm.getFinalTable();
+	log();
 	println(transitionTableToString(pm, sm));
 	println(mergedExtendedToString(pm, sm));
 	pm.reduceExtGrammerFollow();
 	println(extFollowRulesToString(pm, sm));
 	println(normalProductionToString(pm, sm));
 	println(finalTransitionTableToString(pm, sm));
+	*/
 }
