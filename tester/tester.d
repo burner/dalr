806c4e64c139f7432e635688f151707e1de29b38
Robert BuRnEr Schadek <rburners@gmail.com>
2012-02-01 11:19:02 +0100

    save

--- tester/tester.d
+++ tester/tester.d
@@ -3,13 +3,14 @@ import hurt.conv.conv;
 import hurt.container.deque;
 import hurt.container.map;
 import hurt.container.set;
+import hurt.container.isr;
 import hurt.container.dlst;
 import hurt.string.stringutil;
 import hurt.io.stdio;
 import hurt.io.stream;
 import hurt.util.getopt;
 import hurt.util.slog;
-import hurt.util.random;
+import hurt.util.random.random;
 import hurt.string.stringbuffer;
 
 immutable size_t maxdepth = 5;
@@ -21,7 +22,7 @@ void main(string[] args) {
 		~ " will be printed unless a replacedment function is found.\n" ~
 		"This replacement function need to be defined in 'tester/tester.d'.");
 	size_t lexCount = 1;
-	string inputFile = "examplegrammer.dlr";
+	string inputFile = "";
 	string outputFile = "exampleword.d";
 	string startSymbol = "DeclDefsOpt";
 	arguments.setOption("-c", "--count", "of how many lex symbol should " ~
@@ -33,7 +34,72 @@ void main(string[] args) {
 	arguments.setOption("-o", "--output", "outputfile with the created word", 
 		outputFile, true); // the last option
 
-	Twister tw = Twister(123134);
+	if(arguments.wasHelpPrinted() && inputFile == "") {
+		return;
+	}
+
+	Map!(string,string) replace = new Map!(string,string)(ISRType.HashTable);
+	replace.insert("and", "&");
+	replace.insert("andassign", "&=");
+	replace.insert("assign", "=");
+	replace.insert("bang", "!");
+	replace.insert("banggreater", "!>");
+	replace.insert("banggreaterequal", "!>=");
+	replace.insert("bangsmaller", "!<");
+	replace.insert("bangsmallerequal", "!<=");
+	replace.insert("bangsquare", "!<>");
+	replace.insert("bangsquareassign", "!<>=");
+	replace.insert("colon", ":");
+	replace.insert("comma", ",");
+	replace.insert("div", "/");
+	replace.insert("divassign", "/=");
+	replace.insert("dollar", "$");
+	replace.insert("dot", ".");
+	replace.insert("dotdot", "..");
+	replace.insert("dotdotdot", "...");
+	replace.insert("equal", "==");
+	replace.insert("epsilon", "");
+	replace.insert("greater", ">");
+	replace.insert("greaterequal", ">=");
+	replace.insert("lparen", "(");
+	replace.insert("rparen", ")");
+	replace.insert("lbrack", "[");
+	replace.insert("lcurly", "{");
+	replace.insert("rbrack", "]");
+	replace.insert("rcurly", "}");
+	replace.insert("leftshift", "<<");
+	replace.insert("leftshiftassgin", "<<=");
+	replace.insert("less", "<");
+	replace.insert("lessequal", "<=");
+	replace.insert("logicand", "$$");
+	replace.insert("logicor", "||");
+	replace.insert("minus", "-");
+	replace.insert("minusassign", "-=");
+	replace.insert("modassign", "%=");
+	replace.insert("modulo", "%");
+	replace.insert("multassign", "*=");
+	replace.insert("notequal", "!=");
+	replace.insert("or", "|");
+	replace.insert("orassign", "|=");
+	replace.insert("plus", "+");
+	replace.insert("plusassign", "+=");
+	replace.insert("questionmark", "?");
+	replace.insert("rightshift", ">>");
+	replace.insert("rightshiftassgin", ">>=");
+	replace.insert("semicolon", ";");
+	replace.insert("star", "*");
+	replace.insert("tilde", "~");
+	replace.insert("tildeassign", "~=");
+	replace.insert("usignedrightshift", ">>>");
+	replace.insert("usignedrightshiftassign", ">>>=");
+	replace.insert("xor", "^");
+	replace.insert("xorassign", "^=");
+	replace.insert("xorxor", "^^");
+	replace.insert("xorxorassign", "^^=");
+	replace.insert("decrement","--");
+	replace.insert("increment","++");
+
+	Random tw = new Random();
 	//log("%u %s %s %u", lexCount, inputFile, outputFile, tw.next());
 	
 	FileReader fr = new FileReader(inputFile);
@@ -79,16 +145,17 @@ void main(string[] args) {
 	size_t runs = 0;
 	while(cnt < lexCount) {
 		//log("%u", runs);
-		cnt += process(startSymbol, rules, output, curline, tw, keywords, 0);
+		cnt += process(startSymbol, rules, output, curline, tw, keywords, 0, 
+			replace);
 	}
+	log();
 	output.writeLine(curline.getString());
 	output.close();
 }
 
 size_t process(string start, Map!(string,Deque!(Deque!(string))) m, 
-		File output, StringBuffer!(char) curline, Twister tw, 
-		Set!(string) keywords, size_t depth) {
-	tw.seed();
+		File output, StringBuffer!(char) curline, Random tw, 
+		Set!(string) keywords, size_t depth, Map!(string,string) replace) {
 	assert(start !is null);
 	assert(keywords !is null);
 	assert(m !is null);
@@ -104,7 +171,7 @@ size_t process(string start, Map!(string,Deque!(Deque!(string))) m,
 	Deque!(Deque!(string)) list = it.getData();
 
 	// get a random rule and process it
-	size_t whichRule = tw.next() % list.getSize();
+	size_t whichRule = tw.uniform!(size_t)() % list.getSize();
 	assert(whichRule >= 0 && whichRule < 1000);
 	//log("%u %u", whichRule, list.getSize());
 	Deque!(string) rule = list[whichRule];
@@ -122,11 +189,12 @@ size_t process(string start, Map!(string,Deque!(Deque!(string))) m,
 		//log(symbol);
 		if(symbol[0] >= 65 && symbol[0] < 91) {
 			//log("%u %b %b %b %b", depth, symbol is null, m is null, curline is null, keywords is null);
-			count += process(symbol, m, output, curline, tw, keywords, depth++);	
+			count += process(symbol, m, output, curline, tw, keywords, depth++,
+				replace);	
 		} else {
 			//log();
 			count++;
-			curline.pushBack(processString(symbol, keywords, tw));
+			curline.pushBack(processString(symbol, keywords, tw, replace));
 			curline.pushBack(' ');
 			if(curline.getSize() >= 80) {
 				output.writeLine(curline.getString());
@@ -139,30 +207,12 @@ size_t process(string start, Map!(string,Deque!(Deque!(string))) m,
 	return count;
 }
 
-string processString(string str, Set!(string) keywords, Twister tw) {
-	string[string] replace = [ "and": "&", "andassign": "&=", 
-	"assign": "=", "bang": "!", "banggreater": "!>", "banggreaterequal": "!>=",
-	"bangsmaller": "!<", "bangsmallerequal": "!<=", "bangsquare": "!<>",
-	"bangsquareassign": "!<>=", "colon": ":", "comma": ",", "div": "/",
-	"divassign": "/=", "dollar": "$", "dot": ".", "dotdot": "..",
-	"dotdotdot": "...", "equal": "==", "epsilon": "", "greater": ">",
-	"greaterequal": ">=", "lparen": "(", "rparen": ")", "lbrack": "[",
-	"lcurly": "{", "rbrack": "]", "rcurly": "}", "leftshift": "<<",
-	"leftshiftassgin": "<<=", "less": "<", "lessequal": "<=",
-	"logicand": "$$", "logicor": "||", "minus": "-", "minusassign": "-=",
-	"modassign": "%=", "modulo": "%", "multassign": "*=", "notequal": "!=",
-	"or": "|", "orassign": "|=", "plus": "+", "plusassign": "+=",
-	"questionmark": "?", "rightshift": ">>", "rightshiftassgin": ">>=",
-	"semicolon": ";", "star": "*", "tilde": "~", "tildeassign": "~=",
-	"usignedrightshift": ">>>", "usignedrightshiftassign": ">>>=",
-	"xor": "^", "xorassign": "^=",	"xorxor": "^^",	
-	"xorxorassign": "^^=","decrement":"--", "increment":"++"];
-
+string processString(string str, Set!(string) keywords, Random tw, 
+		Map!(string,string) replace) {
 	switch(str) {
 		case "identifier": {
 			StringBuffer!(char) ret = new StringBuffer!(char)();
 			do {
-				tw.seed();
 				ret.clear();
 				size_t len = tw.next() % 14;
 				for(size_t i = 0; i < len; i++) {
@@ -177,7 +227,6 @@ string processString(string str, Set!(string) keywords, Twister tw) {
 			StringBuffer!(char) ret = new StringBuffer!(char)();
 			ret.pushBack("\"");
 			do {
-				tw.seed();
 				ret.clear();
 				size_t len = tw.next() % 14;
 				for(size_t i = 0; i < len; i++) {
@@ -217,8 +266,8 @@ string processString(string str, Set!(string) keywords, Twister tw) {
 			return conv!(uint,string)(tw.next() % 1024) ~ "." ~
 				conv!(uint,string)(tw.next() % 1024);
 		default:
-			if(str in replace) {
-				return replace[str];
+			if(replace.contains(str)) {
+				return replace.find(str).getData();
 			} else {
 				return str;
 			}
