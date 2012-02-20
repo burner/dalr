74f65f2c1953b0de7b8969cdab9f1058c7cd82b1
Robert BuRnEr Schadek <rburners@gmail.com>
2012-02-20 23:41:04 +0100

    token got a member to mark it as a ast node and the position in the ast array

--- arith3test/token.d
+++ arith3test/token.d
@@ -10,13 +10,15 @@ import parsetable;
 
 struct Token {
 	Location loc;
-	int type;
+	int type; // if this is -3 treeIdx gives you the index of the ast node
 	dstring value;
+	size_t treeIdx; // the index of the ast node in the ast array
 
 	this(Location loc, int type, dstring value = "") {
 		this.loc = loc;
 		this.type = type;
 		this.value = value;
+		this.treeIdx = size_t.max;
 	}
 
 	string toString() const {
