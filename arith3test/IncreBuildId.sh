f60edea7442cb8d0c3456092c328b90703d146f5
Robert BuRnEr Schadek <rburners@gmail.com>
2012-02-26 21:52:09 +0100

    forgot another file

---
+++ arith3test/IncreBuildId.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if ! test -f buildinfo.d; then 
+	HASH=`git log -1 --pretty=format:%H`
+	echo -e "module buildinfo;
+
+public static immutable(uint) buildID = 0;
+public static immutable(dstring) gitHash = \"$HASH\";" > buildinfo.d; 
+else
+	TMP=`grep "buildID" buildinfo.d | cut -b 31- | sed 's/\(.*\)./\1/'` 
+	TMP=$(($TMP + 1))
+	HASH=`git log -1 --pretty=format:%H`
+	echo -e "module buildinfo;
+
+public static immutable(uint) buildID = $TMP;
+public static immutable(dstring) gitHash = \"$HASH\";" > buildinfo.d; 
+fi
